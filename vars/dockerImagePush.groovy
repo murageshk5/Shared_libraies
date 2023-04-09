70ea23a01aa719bb46769f5f9645e07295fa9181
Muragesh Kori <murageshkori555@gmail.com>
2023-04-09 19:09:35 +0530

    Docker image

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,12 +1,12 @@
 def call (String dockerhubUser, String ImageName, String ImageTag ) {
 
-    sh """
-        withCredentials([usernameColonPassword(credentialsId: 'dockerhub-auth', variable: 'dockerhubpass')]) {
 
+        withCredentials([usernameColonPassword(credentialsId: 'dockerhub-auth', variable: 'dockerhubpass')]) {
+        sh """
             docker image push ${dockerhubUser}/${ImageName}:${ImageTag}
             docker image push ${dockerhubUser}/${ImageName}:latest
             docker rmi ${dockerhubUser}/${ImageName}:latest
+        """
         }
 
-    """
 }
