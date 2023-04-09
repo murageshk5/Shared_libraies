52c0c1450ac573684c7576a168ee15553ff79bed
Muragesh Kori <murageshkori555@gmail.com>
2023-04-09 19:24:21 +0530

    Docker image

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -2,8 +2,9 @@ def call (String dockerhubUser, String ImageName, String ImageTag ) {
 
 
     withCredentials([usernamePassword(credentialsId: 'dockerhub-auth', passwordVariable: 'DOCKERHUBPASS', usernameVariable: 'DOCKERHUB')]) {
-        
+
         sh """
+            docker login -u $DOCKERHUB -p $DOCKERHUBPASS
             docker image push ${dockerhubUser}/${ImageName}:${ImageTag}
             docker image push ${dockerhubUser}/${ImageName}:latest
             docker rmi ${dockerhubUser}/${ImageName}:latest
