99d9f949fe3d04fbda3f1d3c57304a63ba7596fe
Muragesh Kori <murageshkori555@gmail.com>
2023-04-09 17:27:56 +0530

    Docker build function added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call (String dockerhubUser, String ImageName, String ImageTag ) {
     sh """
 
         docker image build -t ${dockerhubUser}/${ImageName} .
-        docker image tag ${dockerhubUser}/${ImageName}:${ImageTag} 
+        docker image tag ${dockerhubUser}/${ImageName} ${dockerhubUser}/${ImageName}:${ImageTag} 
         docker image tag ${dockerhubUser}/${ImageName} ${dockerhubUser}/${ImageName}:latest
 
     """
