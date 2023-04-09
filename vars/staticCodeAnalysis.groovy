d9d4b1b5e23e264621b5cf4326c0d570ac161fd2
Muragesh Kori <murageshkori555@gmail.com>
2023-04-09 15:39:13 +0530

    static code analysis stage added

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,5 +1,8 @@
-def call () {
+def call (credentialsId) {
 
-    sh 'mvn clean package sonar:sonar'
+    withSonarQubeEnv(credentialsId: credentialsId ) {
 
+            sh 'mvn clean package sonar:sonar'
+
+    }
 }
